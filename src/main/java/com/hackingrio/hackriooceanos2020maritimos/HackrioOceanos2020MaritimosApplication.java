605c62a7b9ba3778f4d7a21838382450a6958210
pricardo.silva <pricardo.silva@sefaz.pb.gov.br>
2020-10-10 13:09:43 -0300

    added backend base code

---
+++ src/main/java/com/hackingrio/hackriooceanos2020maritimos/HackrioOceanos2020MaritimosApplication.java
@@ -0,0 +1,13 @@
+package com.hackingrio.hackriooceanos2020maritimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackrioOceanos2020MaritimosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackrioOceanos2020MaritimosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackingrio/hackriooceanos2020maritimos/HackrioOceanos2020MaritimosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackingrio.hackriooceanos2020maritimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackrioOceanos2020MaritimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
